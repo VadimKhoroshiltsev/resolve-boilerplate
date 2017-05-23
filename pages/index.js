a4f19013b92d0d30f9fd79dc953e9ddb02115d37
VadimKhoroshiltsev <vadim.khoroshiltsev@devexpress.com>
2017-05-23 17:47:51 +0300

    Use withRedux

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,38 @@
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from '../containers/App'
-import reducer from '../reducers'
+import withRedux from 'next-redux-wrapper'
+import Head from 'next/head'
+import {stylesheet} from 'todomvc-app-css/index.css'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import makeStore from '../store'
+import Header from '../components/Header'
+import MainSection from '../components/MainSection'
+import * as TodoActions from '../actions'
 
-const store = createStore(reducer)
-
-export default () => (
-  <Provider store={store}>
-      <App />
-  </Provider>
+const App = ({todos, actions}) => (
+  <div className="todoapp">
+    <Head><style dangerouslySetInnerHTML={{__html: stylesheet}} /></Head>
+    <Header addTodo={actions.addTodo} />
+    <MainSection todos={todos} actions={actions} />
+  </div>
 )
+
+App.propTypes = {
+  todos: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos
+})
+
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(TodoActions, dispatch)
+})
+
+export default withRedux(
+    makeStore,
+    mapStateToProps,
+    mapDispatchToProps
+)(App)
