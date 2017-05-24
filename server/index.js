174f41544e32311c8e06e76cfb0f02d3964d3c02
VadimKhoroshiltsev <vadim.khoroshiltsev@devexpress.com>
2017-05-24 12:36:42 +0300

    Use socket.io

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const next = require("next");
+const http = require("http");
+const socketIO = require("socket.io");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -8,22 +10,29 @@ const handle = app.getRequestHandler();
 app
   .prepare()
   .then(() => {
-    const server = express();
+    const expressApp = express();
+    const server = http.Server(expressApp);
+    const io = socketIO(server);
 
-    server.get("/:filter?", (req, res) => {
+    expressApp.get("/:filter?", (req, res) => {
       const actualPage = "/";
       const queryParams = { filter: req.params.filter };
       app.render(req, res, actualPage, queryParams);
     });
 
-    server.get("*", (req, res) => {
+    expressApp.get("*", (req, res) => {
       return handle(req, res);
     });
 
-    server.listen(3000, err => {
-      if (err) throw err;
-      console.log("> Ready on http://localhost:3000");
+    io.on("connection", function(socket) {
+        console.log('Socket connected');
     });
+
+    server.on("listening", () => {
+      console.log(`Example app listening on port 3000!`);
+    });
+
+    server.listen(3000);
   })
   .catch(ex => {
     console.error(ex.stack);
